ce934b57f157a47a137e671b968072abe252dbfe
baonguyen2310 <lanchinhaicon@gmail.com>
2024-11-08 22:38:40 +0700

    buoi 19

---
+++ Buoi19/cart.js
@@ -0,0 +1,4 @@
+// https://form-test-api.vercel.app/api/cart
+
+// Trang /product/1: cart: POST {userId, productId, quantity}
+// Trang /cart: GET (?userId) , PUT {userId, productId, quantity}, DELETE {userId, productId}
